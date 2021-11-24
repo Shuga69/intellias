72af3ef7dccdff1a13e5dfc33a28dbc6fcdca3bb
vladyslav <vlshugai@gmail.com>
2021-11-24 10:15:25 +0200

    add org chart tree with elements

--- src/components/orgChart/OrgChartComponent.js
+++ src/components/orgChart/OrgChartComponent.js
@@ -2,6 +2,8 @@ import React, { useLayoutEffect, useRef, useEffect } from 'react';
 import { OrgChart } from 'd3-org-chart';
 import ChartElement from "./chartElement/ChartElement";
 import Button from "../button/Button";
+import * as d3 from "d3";
+import './OrgChart.css'
 const OrgChartComponent = (props, ref) => {
   const d3Container = useRef(null);
   let chart = null;
@@ -12,16 +14,56 @@ const OrgChartComponent = (props, ref) => {
       if (!chart) {
         chart = new OrgChart();
       }
+      
       chart
         .container(d3Container.current)
         .data(props.data)
-          .nodeContent((data)=>
+          .nodeContent((d)=>
           {
-            return `<div>${data.data.name}</div`
+            return `  <div style="padding-top:30px;background-color:#fff;margin-left:1px;height:${d.height}px;border-radius:2px;overflow:visible">
+            <div style="height:${d.height - 32}px;padding-top:0px;background-color:white">
+
+
+             <div style="opacity:.2;margin-right:10px;margin-top:15px;float:right">${d.data.id}</div>
+             
+             <div class="${d.data.tags}" style="text-align:center;margin-top:-30px;height:30px;width:${d.width}px">
+               <div style="color:rgb(63, 92, 111);font-size:16px;line-height: 30px"> ${d.data.positionName} </div>
+</div>
+
+             <div style="display:flex;align-items:center;padding:7px 10px;text-align:center">
+              <img src=" ${d.data.imageUrl}" style="margin-right: 5px;border-radius:100px;width:60px;height:60px;" />
+                 <div style="color:rgb(119, 119, 119);font-size:16px;"> ${d.data.name} </div>
+
+             </div> 
+             <div style="display:flex;justify-content:space-between;padding-left:15px;padding-right:15px;opacity: .1">
+               <div > Manages:  ${d.data._directSubordinates}</div>
+               <div > Oversees: ${d.data._totalSubordinates}</div>
+             </div>
+            </div>     
+    </div>`
           })
-        .nodeWidth(d => 200)
-        .nodeHeight(d => 120)
-        .render();
+          .nodeWidth(d => 245)
+          .initialZoom(0.7)
+          .nodeHeight(d => 120)
+          .childrenMargin(d => 40)
+          .compactMarginBetween(d => 15)
+          .compactMarginPair(d => 80)
+          
+          .linkUpdate(function (d, i, arr) {
+            d3.select(this)
+                    .attr("stroke", d => d.data._upToTheRootHighlighted ? '#14760D' : '#c6c6c6')
+                    .attr("stroke-width", d => d.data._upToTheRootHighlighted ? 15 : 1)
+
+            if (d.data._upToTheRootHighlighted) {
+              d3.select(this).raise()
+            }
+          })
+        
+        .render()
+        .expandAll()
+    
+        .fit()
+        
     }
   }, [props.data, d3Container.current]);
 
