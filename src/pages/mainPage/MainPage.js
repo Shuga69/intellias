a3d4c7d3d453f7e67c2e72e0c39de7172d0b33d0
vladyslav <vlshugai@gmail.com>
2021-11-24 10:16:58 +0200

    fix btn color

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -11,7 +11,7 @@ return (
                     <GeneralInfo image={mainImage} title="General Info" text={`Diam sit sed pellentesque porttitor at. Ultrices quisque risus, nisl, sed aliquam faucibus integer. Ornare facilisi morbi eget sagittis. Lorem dictum venenatis mi, diam aliquam justo sit. Nec convallis nisl nec, nulla risus amet diam suspendisse lacus. At nunc fringilla elementum orci. Faucibus vitae blandit laoreet egestas nulla arcu. Viverra id risus at elit risus tortor pharetra in.
                     Porttitor augue vitae congue eget interdum elit, ac. Bibendum nibh lectus ornare nulla natoque. Arcu est amet integer nunc in nec proin. Morbi in hendrerit gravida nec viverra ut. Elit velit, gravida dis vehicula purus sit risus, sed semper. Auctor magna elementum scelerisque vitae commodo tellus sit diam tristique. Nisi condimentum pellentesque ipsum mauris varius ut risus arcu. Amet ullamcorper nulla amet amet.`}/>
                 <div className="feedback-btn-container">
-                <Button type="green" text="Give Feedback"/>
+                <Button type="greenBtn" text="Give Feedback"/>
                 </div>
                 <IntelliasUpdates title="Intellias Updates"/>
                 <div className="feedback-btn-container">
